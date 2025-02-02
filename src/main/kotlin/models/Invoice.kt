96b0bf65000ae66725441af9f50b2cd9ae509852
watabean <watabean@gmail.com>
2025-02-02 19:20:49 +0900

    feat: add data models

---
+++ src/main/kotlin/models/Invoice.kt
@@ -0,0 +1,26 @@
+package com.upsider.models
+
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class Invoice(
+    val id: Int,
+    val userId: Int,
+    val issueDate: LocalDate,
+    val paymentAmount: BigDecimal,
+    val fee: BigDecimal,
+    val feeRate: BigDecimal,
+    val taxAmount: BigDecimal,
+    val taxRate: BigDecimal,
+    val totalAmount: BigDecimal,
+    val paymentDueDate: LocalDate,
+    val createdAt: Long,
+    val updatedAt: Long
+)
+
+data class InvoiceRequest(
+    val userId: Int,
+    val issueDate: LocalDate,
+    val paymentAmount: BigDecimal,
+    val paymentDueDate: LocalDate
+)
---
+++ src/main/kotlin/models/User.kt
@@ -0,0 +1,15 @@
+package com.upsider.models
+
+data class User(
+    val id: Int,
+    val companyName: String,
+    val name: String,
+    val email: String,
+    val password: String,
+    val createdAt: Long,
+    val updatedAt: Long
+)
+
+data class UserRequest(val companyName: String, val name: String, val email: String, val password: String)
+
+data class LoginRequest()

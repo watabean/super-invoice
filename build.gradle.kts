b7aa205115073063058d1bb8799b5cf9dddf729a
watabean <watabean@gmail.com>
2025-02-02 19:21:32 +0900

    chore: add koin for di

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,9 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation("io.insert-koin:koin-ktor:3.5.0")
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+
 }
